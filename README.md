ca52f90da1674d4fb29af046901c8c4d4cdb45e5
Alexander Maricich <w33tmaricich@gmail.com>
2017-04-24 08:36:12 -0400

    feat(all): Displays a random message with ping
    - Makes a noise when a message comes in.
    - Send an ubuntu notification.
    - Picks the message randomly from a list.

---
+++ README.md
@@ -0,0 +1,17 @@
+# posture
+
+Notifications at a given interval to remind me to sit up straight.
+
+## Usage
+
+Dont even bother creating an uberjar. Just clone the repo, then run a
+```
+lein run
+```
+
+## License
+
+Copyright © 2017 Alexander Maricich
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/posture/core.clj
@@ -0,0 +1,31 @@
+(ns posture.core
+  (:use [clojure.java.shell :only [sh]])
+  (:gen-class))
+
+(def NOTIFY-INTERVAL-MINUTES 1)
+(def MESSAGES ["Sit up straight!"
+               "Remember your posture!"
+               "Scoot your butt all the way to the back of your seat."
+               "Dont cross your legs."
+               "Make sure you aren't hunched."
+               "Dont let your headphones pull your head down."
+               "Roll your shoulders back."])
+
+(defn complete-sound
+  "Makes a sound to signify that a notification has gone off."
+  []
+  (sh "paplay" "/usr/share/sounds/freedesktop/stereo/complete.oga"))
+
+(defn notify-send
+  "Sends a notification to the screen in the form of a notification bubble."
+  [text]
+  (complete-sound)
+  (sh "notify-send" text))
+
+(defn -main
+  "Reminds me to check my posture at a given interval of time."
+  [& args]
+  (while true
+    (Thread/sleep (* NOTIFY-INTERVAL-MINUTES 60000))
+    (let [index (rand-int (count MESSAGES))]
+      (notify-send (MESSAGES index)))))
